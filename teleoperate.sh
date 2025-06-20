3a0f75423cdb0d0d9657f791fcf283a7bd22928c
Alexander <alexander.roller12@gmail.com>
2025-06-20 16:30:07 -0700

    customize teleoperate.sh

--- teleoperate.sh
+++ teleoperate.sh
@@ -5,8 +5,8 @@
 
 python -m lerobot.teleoperate \
     --robot.type=so101_follower \
-    --robot.port=/dev/tty.usbmodem58760431541 \
-    --robot.id=my_awesome_follower_arm \
+    --robot.port=/dev/ttyACM0 \
+    --robot.id=follower \
     --teleop.type=so101_leader \
-    --teleop.port=/dev/tty.usbmodem58760431551 \
-    --teleop.id=my_awesome_leader_arm
+    --teleop.port=/dev/ttyACM1 \
+    --teleop.id=leader
